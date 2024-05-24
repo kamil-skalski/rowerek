e352541c80b5b977b39efb6d2d671bab1d07187e
Artur Laskowski <artur.laskowski@comarch.com>
2024-05-24 18:37:50 +0200

    zad1

--- src/main/java/pl/rowerek/reservation/ReservationService.java
+++ src/main/java/pl/rowerek/reservation/ReservationService.java
@@ -2,8 +2,13 @@ package pl.rowerek.reservation;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.rowerek.bike.BikeService;
 import pl.rowerek.common.BikeId;
+import pl.rowerek.common.BikeIsNotAvailableException;
 import pl.rowerek.common.CustomerId;
+import pl.rowerek.customer.CustomerService;
+import pl.rowerek.maintenece.MaintenanceService;
+import pl.rowerek.restriction.RestrictionService;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -14,9 +19,26 @@ public class ReservationService {
 
     private final ReservationRepository reservationRepository;
     private final ReservationProperties reservationProperties;
+    private final BikeService bikeService;
+    private final CustomerService customerService;
+    private final MaintenanceService maintenanceService;
+    private final RestrictionService restrictionService;
 
     ReservationId reserve(BikeId bikeId, CustomerId customerId, Instant when) {
-        //TODO
+        bikeService.checkBikeExists(bikeId);
+        customerService.checkCustomerExists(customerId);
+
+        if (isBikeReserved(bikeId, when)) {
+            throw new BikeIsNotAvailableException("Bike is already reserved: BikeId: " + bikeId.id());
+        }
+
+        if (maintenanceService.existsActiveMaintenanceByBikeId(bikeId)) {
+            throw new BikeIsNotAvailableException("Bike is currently undergoing maintenance: BikeId: " + bikeId.id());
+        }
+
+        if (restrictionService.isInRestriction(bikeId, when)) {
+            throw new BikeIsNotAvailableException("Bike is currently excluded due to external restrictions: BikeId: " + bikeId.id());
+        }
 
         var newReservation = new Reservation(bikeId, customerId, when.plus(Duration.ofMinutes(reservationProperties.getDurationMinutes())));
         return reservationRepository.save(newReservation).getReservationId();
